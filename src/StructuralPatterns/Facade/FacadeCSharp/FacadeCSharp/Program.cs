089170895a195a222eca47f0638a04c6cf6828f6
ogycode <verlokav@gmail.com>
2018-07-04 14:07:31 +0300

    Add FacadeCSharp.

---
+++ src/StructuralPatterns/Facade/FacadeCSharp/FacadeCSharp/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeCSharp
+{
+    //Первая подсистема
+    class Welcome
+    {
+        public Welcome() { }
+
+        public void DrawStart() => Console.WriteLine("#  << Start    #");
+        public void DrawResume() => Console.WriteLine("#  << Resume   #");
+        public void DrawLoad() => Console.WriteLine("#  << Load     #");
+        public void DrawSave() => Console.WriteLine("#  << Save     #");
+        public void DrawOptions() => Console.WriteLine("#  << Options  #");
+        public void DrawExit() => Console.WriteLine("#  << Exit     #");
+        public void DrawBack() => Console.WriteLine("#  << Back     #");
+    };
+
+    //Вторая подсистема, может быть большое количество
+    class Decoration
+    {
+        public Decoration() { }
+
+        public void DrawEmptyLine() => Console.WriteLine("#              #");
+        public void DrawWallLine() => Console.WriteLine("################");
+    };
+
+    //Непосредственно фасад
+    class Menu
+    {
+        Welcome w;
+        Decoration d;
+
+        public Menu()
+        {
+            w = new Welcome();
+            d = new Decoration();
+        }
+
+        public void DrawStartMenu()
+        {
+            d.DrawWallLine();
+            d.DrawEmptyLine();
+            Console.WriteLine("#   Start      #");
+            d.DrawEmptyLine();
+
+            w.DrawStart();
+            w.DrawLoad();
+            w.DrawOptions();
+            w.DrawExit();
+
+            d.DrawEmptyLine();
+            d.DrawWallLine();
+        }
+        public void DrawInGameMenu()
+        {
+            d.DrawWallLine();
+            d.DrawEmptyLine();
+            Console.WriteLine("#   In Game    #");
+            d.DrawEmptyLine();
+
+            w.DrawResume();
+            w.DrawSave();
+            w.DrawLoad();
+            w.DrawOptions();
+            w.DrawBack();
+
+            d.DrawEmptyLine();
+            d.DrawWallLine();
+        }
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Facade";
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Menu m = new Menu();
+
+            m.DrawStartMenu();
+
+            Console.WriteLine("\n\n\n");
+
+            m.DrawInGameMenu();
+
+            Console.Read();
+        }
+    }
+}
--- src/StructuralPatterns/Facade/README.md
+++ src/StructuralPatterns/Facade/README.md
@@ -1,4 +1,4 @@
-[![Logo](https://raw.githubusercontent.com/ogycode/DesignPatterns/master/merch/decorator.PNG)](https://github.com/ogycode/DesignPatterns/tree/master/src/StructuralPatterns/Facade)
+[![Logo](https://raw.githubusercontent.com/ogycode/DesignPatterns/master/merch/facade.PNG)](https://github.com/ogycode/DesignPatterns/tree/master/src/StructuralPatterns/Facade)
 
-# Facade [[C++]()/[C#]()]
+# Facade [[C++](https://github.com/ogycode/DesignPatterns/blob/master/src/StructuralPatterns/Facade/FacadeCPP/FacadeCPP/FacadeCPP.cpp)/[C#]()]
 Фасад — структурный шаблон проектирования, позволяющий скрыть сложность системы путём сведения всех возможных внешних вызовов к одному объекту, делегирующему их соответствующим объектам системы. [WIKI](https://ru.wikipedia.org/wiki/%D0%A4%D0%B0%D1%81%D0%B0%D0%B4_(%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD_%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F))
