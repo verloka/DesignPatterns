98ce9c204b22de5e271d255f8d12dbf05cb4c7d1
ogycode <verlokav@gmail.com>
2018-07-16 15:26:40 +0300

    Add MediatorCSharp

---
+++ src/BehavioralPatterns/Mediator/MediatorCSharp/MediatorCSharp/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorCSharp
+{
+    //Интерфейс медиатора
+    interface IMediator
+    {
+        void Send(string msg, IColleague coll);
+    }
+
+    //Интерфейс обькта между которыми будет происходить общение
+    interface IColleague
+    {
+        IMediator med { get; set; }
+
+        void SetMediator(IMediator med);
+    }
+
+    //Конкретный обект общения
+    class ColleagueJack : IColleague
+    {
+        public IMediator med { get; set; }
+
+        public void SetMediator(IMediator med) => this.med = med;
+
+        public void Send(string msg) => med.Send(msg, this);
+        public void Notify(string msg) => Console.WriteLine($"Jack gets message: '{msg}'");
+    }
+
+    //Конкретный обект общения
+    class ColleagueJhon : IColleague
+    {
+        public IMediator med { get; set; }
+
+        public void SetMediator(IMediator med) => this.med = med;
+
+        public void Send(string msg) => med.Send(msg, this);
+        public void Notify(string msg) => Console.WriteLine($"Jhon gets message: '{msg}'");
+    }
+
+    //Реализация конкретного посредника в общении между двумя обьектами
+    class MediatorJackJson : IMediator
+    {
+        public ColleagueJack Jack { get; set; }
+        public ColleagueJhon Jhon { get; set; }
+
+        public void Send(string msg, IColleague coll)
+        {
+            if (coll == Jack)
+                Jhon.Notify(msg);
+            else if (coll == Jhon)
+                Jack.Notify(msg);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Mediator";
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            MediatorJackJson mjj = new MediatorJackJson();
+
+            ColleagueJack jack = new ColleagueJack()
+            {
+                med = mjj
+            };
+            ColleagueJhon jhon = new ColleagueJhon()
+            {
+                med = mjj
+            };
+
+            mjj.Jack = jack;
+            mjj.Jhon = jhon;
+
+            jack.Send("Hello, what's up?");
+            jhon.Send("Hi, im fine, and you?");
+
+            Console.Read();
+        }
+    }
+}
--- src/BehavioralPatterns/Mediator/README.md
+++ src/BehavioralPatterns/Mediator/README.md
@@ -1,4 +1,4 @@
 [![Logo](https://raw.githubusercontent.com/ogycode/DesignPatterns/master/merch/mediator.PNG)](https://github.com/ogycode/DesignPatterns/tree/master/src/BehavioralPatterns/Mediator)
 
-# Mediator [[C++]()/[C#]()]
+# Mediator [[C++](https://github.com/ogycode/DesignPatterns/blob/master/src/BehavioralPatterns/Mediator/MediatorCPP/MediatorCPP/MediatorCPP.cpp)/[C#]()]
 Посредник — поведенческий шаблон проектирования, обеспечивающий взаимодействие множества объектов, формируя при этом слабую связанность и избавляя объекты от необходимости явно ссылаться друг на друга. [WIKI](https://ru.wikipedia.org/wiki/%D0%9F%D0%BE%D1%81%D1%80%D0%B5%D0%B4%D0%BD%D0%B8%D0%BA_(%D1%88%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD_%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D1%8F))
